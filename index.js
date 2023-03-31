b4ce6b2349b4360f443367da62b5ded3f13514b8
Zaid Zaihan <zaidzaihan1611@gmail.com>
2023-03-31 17:41:12 +0800

    Trying to implement basic login ui for learning purposes

--- index.js
+++ index.js
@@ -13,21 +13,22 @@ let dbUsers = [{
 ]
 
 function login(username, password){
-console.log("Someone try to login with", username, password)
+//console.log("Someone try to login with", username, password)
 let matched = dbUsers.find(element => 
     element.username == username
 )
 //console.log(matched)
 if (matched){
     if(matched.password == password){
-    return matched}
+    window.location.href = "Welcome.html"}
     else {
-        return "Password not matched"
+        document.getElementById("prompt").innerHTML = "Wrong Password!";
     }
 }
 else{
-    return "Username not found!"
+    document.getElementById("prompt").innerHTML = "Username Not Registered. Please register!";
 }
+console.log(dbUsers)
 }
 
 function register(newusername, newpassword, newname, newEmail){
@@ -36,7 +37,7 @@ function register(newusername, newpassword, newname, newEmail){
         element.username == newusername
     )
     if(exist){
-        return "Username already exist"
+        document.getElementById("prompt").innerHTML = "Username Already Exist";
     }
         else{
             dbUsers.push({
@@ -45,11 +46,33 @@ function register(newusername, newpassword, newname, newEmail){
                 name: newname,
                 email: newEmail
             })
+            document.getElementById("prompt").innerHTML = "Successfully registered!";
+            console.log(dbUsers)
         }
 }
 
+function loginFunc(){
+    let name = document.getElementById("fname").value;
+    console.log(name)
+    let password = document.getElementById("fpassword").value;
+    console.log(password)
+    //call function
+    login(name, password);
+}
+
+function registerFunc(){
+    let name = document.getElementById("rname").value;
+    let password = document.getElementById("rpassword").value;
+    let nickname = document.getElementById("rnickname").value;
+    let email = document.getElementById("remail").value;
+    //call function
+    register(name, password, nickname, email);
+}
+
+
+
 //try to login
-console.log(login("Zaid", "password"))
-console.log(login("Abu", "hello"))
-register("abu", "hello", "Abu Gaming", "AbuGaming@gmail.com")
-console.log(login("abu", "hello"))
+// console.log(login("Zaid", "password"))
+// console.log(login("Abu", "hello"))
+// register("abu", "hello", "Abu Gaming", "AbuGaming@gmail.com")
+// console.log(login("abu", "hello"))
